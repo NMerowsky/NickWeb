9a32827eeed6b70935c66e8d905dfb31cfa12d2c
Nick Merowsky <nmerowsky@gmail.com>
2017-09-11 16:07:41 -0400

    starting nav bar play

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,15 +2,26 @@
 <html>
 <head>
 	<title>
+		{{-- Tab Title --}}
 		@yield('title')
 	</title>
+
+	{{-- Bootstrap core CSS --}}
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+	{{-- Independent Header --}}
 	@yield('header')
 </head>
 <body>
+	{{-- Nav Bar --}}
+	@include('navigation.nav')
+
+	{{-- Content --}}
 	<div class="container">
 		@yield('content')
 	</div>
 
+	{{-- Footer --}}
 	@yield('footer')
 </body>
 </html>
